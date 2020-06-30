fe072b8a76d67e224322ad0af19f7e1011848b44
EasyWin6 <sunshineregi888@gmail.com>
2020-06-30 14:32:38 +0300

    11

---
+++ cmake-build-debug/CMakeFiles/uchat2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uchat2.dir/server/main.c.o"
+  "uchat2"
+  "uchat2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/uchat2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ server/inc/server.h
@@ -0,0 +1,18 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/errno.h>
+#include <pthread.h>
+#include <netdb.h>
+
+int main(int argc, const char **argv);
+
+#endif
